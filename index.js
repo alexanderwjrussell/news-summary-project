93e8580014bfadcc6d7ef4d7b685b35a6fafb0e3
david anderson <d.a.m.anderson@gmail.com>
2016-11-10 12:17:11 +0000

    prints out html list of headlines

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var xhr = new XMLHttpRequest();
-xhr.open('GET', "http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/politics/blog/2014/feb/17/alex-salmond-speech-first-minister-scottish-independence-eu-currency-live?show-fields=body", true);
+xhr.open('GET', "http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?order-by=newest&q=politics?show-fields=body", true);
 xhr.send();
 
 xhr.onreadystatechange = processRequest;
 
-function processRequest (e) {
+function processRequest(e) {
   if (xhr.readyState == 4 && xhr.status == 200) {
     var response = JSON.parse(xhr.responseText);
-    console.log(response.content.webTitle);
+    var array = response.response.results
+    var headlinesListView = new HeadlinesListView(array);
+    document.getElementById("content").innerHTML = headlinesListView.getHTML();
   }
-}
+};
